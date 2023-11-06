83dc163f9b59aff903d127e2506f01965362ffb5
JunaGanz <93413322+JunaGanz@users.noreply.github.com>
2023-11-06 11:28:30 +0700

    Add files via upload

--- public/src/dataStatic.js
+++ public/src/dataStatic.js
@@ -4,8 +4,9 @@ import imgSmk01c from '@/public/src/paslon-smk01.c.png'
 import imgSmk02a from '@/public/src/paslon-smk02.a.png'
 import imgSmk02b from '@/public/src/paslon-smk02.b.png'
 import imgSmk02c from '@/public/src/paslon-smk02.c.png'
-import imgSmp01a from '@/public/src/paslon-smp02.a.png'
-import imgSmp01b from '@/public/src/paslon-smp02.b.png'
+import imgSmp01a from '@/public/src/paslon-smp01.a.png'
+import imgSmp01b from '@/public/src/paslon-smp01.b.png'
+import imgSmp01c from '@/public/src/paslon-smp01.c.png'
 import imgSmp02a from '@/public/src/paslon-smp02.a.png'
 import imgSmp02b from '@/public/src/paslon-smp02.b.png'
 import imgSmp03a from '@/public/src/paslon-smp03.a.png'
@@ -18,7 +19,7 @@ const dataStatic = {
          "wakil": "Delia Nahda",
          "visi": "Menjadi OSIS sebagai wadah yang mampu segala aspirasi, baca, potensi, dan kreativitas, serta memajukan ilmu pengetahuan dan teknologi (iptek) dan berakhlak mulia dikalang siswa yang berlandaskan Pancasila.",
          "misi": "Meningkatkan keimanan dan ketakwaan kepada Tuhan Yang Maha Esa\nMengadakan suatu kegiatan yang berguna untuk pengembangan bakat, potensi, dan kreativitas para siswa\nRutin menjalankan pelatihan demi memajukan ilmu pengetahuan dan teknologi dikalang para siswa\nMenjalankan suatu kegiatan bela negara dilingkungan sekolah dan rumah\nMenjalin hubungan yang harmonis dengan semua pihak yang terikat dalam seluruh kegiatan OSIS.",
-         "image": [imgSmp01a, imgSmp01b]
+         "image": [imgSmp01a, imgSmp01b, imgSmp01c]
       },
       "02": {
          "ketua": "Agnes",
